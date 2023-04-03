c92251ce5512e3874ba6e51dd4813d559d31850a
Ryan Meneses <58452495+hiyaryan@users.noreply.github.com>
2023-04-03 00:39:41 +0000

    Add images and location to header and portfolio.

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,10 @@
   <div class="landing-page" id="home">
     <NavBar :pageContent="pageContent" />
     <div class="header">
+      <img :src="`/img/me.png`" alt="" />
       <h1>{{ name }}</h1>
       <h2>{{ title }}</h2>
+      <h5> {{ location }} </h5>
       <p>{{ tagline }}</p>
     </div>
     <div class="section" id="about">
@@ -14,7 +16,7 @@
       <h2>Portfolio</h2>
       <div class="portfolio">
         <div v-for="project in portfolio" :key="project.name">
-          <img :src="`@/assets/img/${project.thumbnail}`" alt="" />
+          <img :src="`/img/${project.thumbnail}`" alt="" />
           <h3>{{ project.name }}</h3>
           <p>{{ project.description }}</p>
         </div>
@@ -22,7 +24,9 @@
     </div>
     <div class="footer" id="contact">
       <h2>Contact</h2>
-      <p>{{ contact }} <a :href="`${linkedin}`" target="_blank">LinkedIn</a></p>
+      <p>{{ contact }}
+        <a :href="`${linkedin}`" target="_blank">LinkedIn</a> and <a :href="`${github}`" target="_blank">GitHub</a>
+      </p>
     </div>
   </div>
 </template>
@@ -46,6 +50,7 @@ export default {
       name: 'Ryan Meneses',
       title: 'Software Engineer',
       tagline: 'Passionate about solving problems that lead to a better world.',
+      location: 'San Francisco Bay Area',
       about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rhoncus ipsum sit amet laoreet maximus. Suspendisse vel lacinia ipsum. Duis ullamcorper est eget tellus pulvinar, vel malesuada tellus hendrerit. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rhoncus ipsum sit amet laoreet maximus. Suspendisse vel lacinia ipsum. Duis ullamcorper est eget tellus pulvinar, vel malesuada tellus hendrerit. Nulla facilisi.',
       portfolio: [
         {
@@ -65,7 +70,8 @@ export default {
         }
       ],
       contact: 'Find me on ',
-      linkedin: 'https://www.linkedin.com/in/ryan-james-meneses-b6973a132'
+      linkedin: 'https://www.linkedin.com/in/ryan-james-meneses-b6973a132',
+      github: 'https://github.com/hiyaryan',
     }
   }
 }
@@ -83,15 +89,28 @@ export default {
 
 .header {
   text-align: center;
-  margin-top: 25px;
+  margin-top: 50px;
   margin-bottom: 25px;
+}
+
+.header img {
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  margin-bottom: -50px;
 }
 
 .header h1 {
   color: rgba(0, 0, 0, 0.850);
   padding-top: 50px;
+  padding-bottom: 10px;
   font-size: 40px;
   font-weight: 600;
+}
+
+.header p {
+  margin-top: 50px;
 }
 
 .portfolio {
@@ -101,13 +120,17 @@ export default {
 }
 
 .portfolio div {
+  text-align: center;
+  align-items: center;
+  word-wrap: break-word;
   width: calc(33.33% - 10px);
   margin-bottom: 20px;
 }
 
 .portfolio img {
-  width: 100%;
-  height: auto;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
   margin-bottom: 10px;
 }
 
