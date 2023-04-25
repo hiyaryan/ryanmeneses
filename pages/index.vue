73d4d147790b4eabfed012bbb122b93ea3285e56
Ryan Meneses <rmenese1@asu.edu>
2023-04-25 13:43:54 -0700

    Move static text into static dir, organize code.

--- pages/index.vue
+++ pages/index.vue
@@ -39,6 +39,7 @@
 
 <script>
 import NavBar from '~/components/NavBar.vue'
+import content from '~/static/content.js'
 
 export default {
   head() {
@@ -52,32 +53,8 @@ export default {
   },
 
   beforeMount() {
-    // Fetch data from public APIs
-    fetch("https://type.fit/api/quotes")
-      .then(async response => {
-        const data = await response.json();
-
-        // check for error response
-        if (!response.ok) {
-          // get error message from body or default to response statusText
-          const error = (data && data.message) || response.statusText;
-          return Promise.reject(error);
-        }
-
-        let quote = this.get_random_quote(data);
-
-        this.quote.text = quote.text;
-
-        if (quote.author) {
-          this.quote.author = quote.author;
-        } else {
-          this.quote.author = "Unknown";
-        }
-      })
-      .catch(error => {
-        this.errorMessage = error;
-        console.error("Error fetching quote from https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373", error);
-      });
+    // Get quote from public API
+    this.getQuote()
   },
 
   data() {
@@ -88,43 +65,50 @@ export default {
         { id: 'projects', title: 'Projects' },
         { id: 'contact', title: 'Contact' },
       ],
-      name: 'Ryan Meneses',
-      title: 'Software Engineer',
-      tagline: 'Passionate about solving problems that lead to a better world.',
-      location: 'San Francisco Bay Area',
-      about: ["Hello there! Thank you for visiting my website! My name is Ryan, and I'm a software engineer who is passionate about using technology to help solve big questions and push humanity towards our maximum potential.", "Ever since I was a kid, I've been fascinated by the mysteries of the universe and the big questions that we as humans seek to answer. And as I grew older, I realized that technology could play a crucial role in helping us unravel some of these mysteries and push the boundaries of what we thought was possible.", "In my career as a software engineer, I've been fortunate to work on some incredible projects with some amazing people who have only fueled my desire to work on these types of problems. One project, for instance, is all about pushing humanity closer towards becoming an interplanetary species, while another deals with humanity's ability to harness the computational power of the atomic world.", "What excites me most about my work is the potential for technology to help us unlock new insights and discoveries that can push humanity towards its maximum potential. Whether we're developing new algorithms to help us understand the fundamental workings of the universe or creating tools to help us harness the power of artificial intelligence, I believe that we can make a real difference by working together to push the boundaries of what we thought was possible.", "When I'm not coding, I love exploring these spaces through the lenses of those outside of my industry. I'm very curious to see the workings and ideas of those approaching these problems from different angles. I'm also very interested in hearing the kinds of impacts new technologies and discoveries might pose on humanity alike.", "Overall, I'm thrilled to be a part of a community of thinkers and doers who are just as passionate as I am about using technology to make a positive impact on the world. So, let's join forces and keep pushing the boundaries together!"],
-      quote: { 'text': 'We are a way for the universe to know itself.', "author": 'Carl Sagan' },
-      projects: [
-        {
-          name: 'SIMOC SAM',
-          thumbnail: 'project-1-thumbnail.png',
-          description: ["At Over the Sun, I extended SIMOC, a habitat atmospheric and environmental simulation hosted on NGS, enabling citizen scientists and classrooms to access a live data feed from SAM, a real-life hermetically sealed habitat, using Vue.js for the front-end and Python for the back-end. My role involved designing and implementing features to allow users to view and analyze data from Adafruit sensors measuring CO2, humidity, temperature, pressure, and VOC levels. The project's challenge was to create a user-friendly interface with data visualization tools, and the outcome was a contribution to space exploration and education."],
-          ref: 'https://github.com/overthesun',
-        },
-        {
-          name: 'QCS Quantum Circuits',
-          thumbnail: 'project-2-thumbnail.png',
-          description: ["At Rigetti Computing, I spearheaded a Rust-based tool that generates LaTeX-formatted quantum circuits, which are graphical representations of quantum systems used to visualize the flow of quantum information. The tool serializes Quil, the assembly language of quantum computers, into LaTeX form, and is accessible via the company's quantum cloud services (QCS). The tool provides a high-level abstraction of the structure of quantum instructions. This aids designers and researchers to analyze and design complex quantum algorithms before accessing valuable quantum computing resources."],
-          ref: 'https://github.com/rigetti/quil-rs/pull/145'
-        },
-        {
-          name: 'My Website',
-          thumbnail: 'project-3-thumbnail.png',
-          description: ["I'm currently building my portfolio website using Vue.js and Nuxt.js, and I'm excited to share my journey with you. One of the reasons I decided to build this website publicly is that I wanted to showcase my skills and progress as a software engineer. By building this website in public, I'm able to share my coding process, learn from feedback, and connect with other developers. A new challenge I'm facing is building a website layout, style, and user experience. Overall, I'm proud of what I've accomplished so far, and I'm looking forward to sharing more updates on this project as I continue to develop and improve my skills."],
-          ref: 'https://github.com/hiyaryan/ryanmeneses',
-        }
-      ],
-      contact: 'Find me on ',
-      linkedin: 'https://www.linkedin.com/in/ryan-james-meneses',
-      github: 'https://github.com/hiyaryan',
+      name: content.name,
+      title: content.title,
+      tagline: content.tagline,
+      location: content.location,
+      about: content.about,
+      quote: content.quote,
+      projects: content.projects,
+      contact: content.contact,
+      linkedin: content.linkedin,
+      github: content.github,
       year: new Date().getFullYear()
     }
   },
 
   methods: {
+    getQuote() {
+      fetch("https://type.fit/api/quotes")
+        .then(async response => {
+          const data = await response.json()
+
+          // check for error response
+          if (!response.ok) {
+            // get error message from body or default to response statusText
+            const error = (data && data.message) || response.statusText
+            return Promise.reject(error)
+          }
+
+          let quote = this.get_random_quote(data)
+          this.quote.text = quote.text
+
+          if (quote.author) {
+            this.quote.author = quote.author
+          } else {
+            this.quote.author = "Unknown"
+          }
+        }).catch(error => {
+          this.errorMessage = error
+          console.error("Error fetching quote from https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373", error)
+        })
+    },
+
     // Returns a random quote from a dictionary of quotes fetched from the public quotes API: https://type.fit/api/quotes
     get_random_quote(quotes) {
-      let index = Math.random() * quotes.length;
+      let index = Math.random() * quotes.length
       return quotes[Math.ceil(index)]
     }
   },
---
+++ static/content.js
@@ -0,0 +1,37 @@
+export default {
+    pageContent: [
+        { id: 'home', title: 'Home' },
+        { id: 'about', title: 'About' },
+        { id: 'projects', title: 'Projects' },
+        { id: 'contact', title: 'Contact' },
+    ],
+    name: 'Ryan Meneses',
+    title: 'Software Engineer',
+    tagline: 'Passionate about solving problems that lead to a better world.',
+    location: 'San Francisco Bay Area',
+    about: ["Hello there! Thank you for visiting my website! My name is Ryan, and I'm a software engineer who is passionate about using technology to help solve big questions and push humanity towards our maximum potential.", "Ever since I was a kid, I've been fascinated by the mysteries of the universe and the big questions that we as humans seek to answer. And as I grew older, I realized that technology could play a crucial role in helping us unravel some of these mysteries and push the boundaries of what we thought was possible.", "In my career as a software engineer, I've been fortunate to work on some incredible projects with some amazing people who have only fueled my desire to work on these types of problems. One project, for instance, is all about pushing humanity closer towards becoming an interplanetary species, while another deals with humanity's ability to harness the computational power of the atomic world.", "What excites me most about my work is the potential for technology to help us unlock new insights and discoveries that can push humanity towards its maximum potential. Whether we're developing new algorithms to help us understand the fundamental workings of the universe or creating tools to help us harness the power of artificial intelligence, I believe that we can make a real difference by working together to push the boundaries of what we thought was possible.", "When I'm not coding, I love exploring these spaces through the lenses of those outside of my industry. I'm very curious to see the workings and ideas of those approaching these problems from different angles. I'm also very interested in hearing the kinds of impacts new technologies and discoveries might pose on humanity alike.", "Overall, I'm thrilled to be a part of a community of thinkers and doers who are just as passionate as I am about using technology to make a positive impact on the world. So, let's join forces and keep pushing the boundaries together!"],
+    quote: { 'text': 'We are a way for the universe to know itself.', "author": 'Carl Sagan' },
+    projects: [
+        {
+            name: 'SIMOC SAM',
+            thumbnail: 'project-1-thumbnail.png',
+            description: ["At Over the Sun, I extended SIMOC, a habitat atmospheric and environmental simulation hosted on NGS, enabling citizen scientists and classrooms to access a live data feed from SAM, a real-life hermetically sealed habitat, using Vue.js for the front-end and Python for the back-end. My role involved designing and implementing features to allow users to view and analyze data from Adafruit sensors measuring CO2, humidity, temperature, pressure, and VOC levels. The project's challenge was to create a user-friendly interface with data visualization tools, and the outcome was a contribution to space exploration and education."],
+            ref: 'https://github.com/overthesun',
+        },
+        {
+            name: 'QCS Quantum Circuits',
+            thumbnail: 'project-2-thumbnail.png',
+            description: ["At Rigetti Computing, I spearheaded a Rust-based tool that generates LaTeX-formatted quantum circuits, which are graphical representations of quantum systems used to visualize the flow of quantum information. The tool serializes Quil, the assembly language of quantum computers, into LaTeX form, and is accessible via the company's quantum cloud services (QCS). The tool provides a high-level abstraction of the structure of quantum instructions. This aids designers and researchers to analyze and design complex quantum algorithms before accessing valuable quantum computing resources."],
+            ref: 'https://github.com/rigetti/quil-rs/pull/145'
+        },
+        {
+            name: 'My Website',
+            thumbnail: 'project-3-thumbnail.png',
+            description: ["I'm currently building my portfolio website using Vue.js and Nuxt.js, and I'm excited to share my journey with you. One of the reasons I decided to build this website publicly is that I wanted to showcase my skills and progress as a software engineer. By building this website in public, I'm able to share my coding process, learn from feedback, and connect with other developers. A new challenge I'm facing is building a website layout, style, and user experience. Overall, I'm proud of what I've accomplished so far, and I'm looking forward to sharing more updates on this project as I continue to develop and improve my skills."],
+            ref: 'https://github.com/hiyaryan/ryanmeneses',
+        }
+    ],
+    contact: 'Find me on ',
+    linkedin: 'https://www.linkedin.com/in/ryan-james-meneses',
+    github: 'https://github.com/hiyaryan',
+}
