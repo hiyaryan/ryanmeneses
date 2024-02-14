2996e8ad6344d302d58284a4c994ca2dc1545c6c
hiyaryan <meneses.ryan.j@gmail.com>
2024-02-13 18:37:01 -0800

    Add custom hook to fetch notion data by database id.

---
+++ src/hooks/useNotionData.jsx
@@ -0,0 +1,17 @@
+// Custom hook to fetch Notion data.
+export function useNotionData() {
+    async function fetchNotionData(databaseId) {
+        try {
+            const endpoint = `${ process.env.NEXT_PUBLIC_LAMBDA_URL }/path?databaseId=${ databaseId }`;
+            const res = await fetch(endpoint);
+            const data = await res.json();
+
+            return data;
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    return fetchNotionData;
+}
