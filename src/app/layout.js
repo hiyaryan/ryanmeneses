919fce73126b8f9235a918c7d87932f1b1251cda
hiyaryan <meneses.ryan.j@gmail.com>
2024-02-14 19:32:29 -0800

    Move scope of theme and css baseline to top level in layout.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,10 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@mui/material/styles";
+import defaultTheme from "../styles/theme";
+import CssBaseline from "@mui/material/CssBaseline";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,9 +18,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AppRouterCacheProvider>
-          {children}
-        </AppRouterCacheProvider>
+        <ThemeProvider theme={defaultTheme}>
+          <CssBaseline />
+          <AppRouterCacheProvider>
+            {children}
+          </AppRouterCacheProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -2,27 +2,19 @@ import NavBar from "../components/NavBar";
 import Profile from "../components/Body/Profile";
 import About from "../components/Body/About";
 import StickyFooter from "../components/StickyFooter";
-import { ThemeProvider } from "@mui/material/styles";
-import defaultTheme from "../styles/theme";
-import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 
 export default function Home() {
   return (
-    <>
-      <ThemeProvider theme={defaultTheme}>
-        <CssBaseline />
-        <Box sx={{
-          display: "flex",
-          flexDirection: "column",
-          minHeight: "100vh"
-        }} >
-          <NavBar />
-          <Profile />
-          <About />
-          <StickyFooter />
-        </Box>
-      </ThemeProvider>
-    </>
+    <Box sx={{
+      display: "flex",
+      flexDirection: "column",
+      minHeight: "100vh"
+    }} >
+      <NavBar />
+      <Profile />
+      <About />
+      <StickyFooter />
+    </Box>
   );
 }
