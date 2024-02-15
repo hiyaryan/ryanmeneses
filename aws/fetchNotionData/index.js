7673acd075fe109c96d22c082c2abedf7cef47ba
hiyaryan <meneses.ryan.j@gmail.com>
2024-02-14 22:40:21 -0800

    Find database by query string parameter.

--- aws/fetchNotionData/index.js
+++ aws/fetchNotionData/index.js
@@ -2,23 +2,38 @@ const { Client } = require("@notionhq/client");
 
 exports.handler = async function (event) {
   const notion = new Client({ auth: process.env.NOTION_API_KEY });
-  const databaseId = process.env.NOTION_DB;
+  const databaseId = event.queryStringParameters.databaseId;
+
+  if (!databaseId) {
+    return {
+      statusCode: 400,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ error: "No database ID provided" }),
+    };
+  }
 
   try {
-    const response = await notion.databases.retrieve({
-      database_id: databaseId,
-    });
+    const response = await notion.databases.query({ database_id: databaseId });
     return {
       statusCode: 200,
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
       },
-      body: JSON.stringify(response),
+      body: JSON.stringify(response.results[0].properties),
     };
   } catch (error) {
     console.error(error);
     return {
       statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
       body: JSON.stringify({ error: "Failed to fetch data from Notion API" }),
     };
   }
