4fd27588d63b68bf2f890b72d38c4f265cc5d537
hiyaryan <meneses.ryan.j@gmail.com>
2024-02-14 19:21:03 -0800

    Set app icon in app directory.

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,21 +7,12 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ryan's Brew-cessities",
-  icons: {
-    icon: '/icon.svg',
-  },
   description: "Ryan Meneses' personal website and portfolio.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <title>{metadata.title}</title>
-        <link rel="icon" href={metadata.icons.icon} />
-        <meta name="description" content={metadata.description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
       <body className={inter.className}>
         <AppRouterCacheProvider>
           {children}
